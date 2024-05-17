fd68356198b65cdf7e1a569cf85726c1e02714fb
MuttonString <16234921893@qq.com>
2024-05-17 15:35:19 +0800

    移除mock

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { ConfigEnv, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }: ConfigEnv) => {
@@ -12,10 +11,6 @@ export default defineConfig(({ command }: ConfigEnv) => {
             createSvgIconsPlugin({
                 iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
                 symbolId: 'icon-[dir]-[name]'
-            }),
-            viteMockServe({
-                mockPath: 'mock',
-                enable: command === 'serve'
             })
         ],
         resolve: {
